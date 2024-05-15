8df04cdfd6d58971ce115d32b9b7fd0de4fa1c51
hanyhesham <hhesham.elsaeed@gmail.com>
2024-05-15 12:36:28 +0300

    add iam role for ebs csi

--- terraform/eks/eks.tf
+++ terraform/eks/eks.tf
@@ -41,7 +41,8 @@ module "eks" {
       most_recent = true
     }
     aws-ebs-csi-driver = {
-      most_recent = true
+      service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
+      most_recent              = true
     }
   }
 
@@ -110,5 +111,20 @@ resource "aws_security_group" "additional" {
       "172.16.0.0/12",
       "192.168.0.0/16",
     ]
+  }
+}
+
+
+module "ebs_csi_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name             = "${var.eks_name}-ebs-csi"
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
   }
 }
